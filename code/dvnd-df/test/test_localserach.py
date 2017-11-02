1b5a1e62c39bc9716263374da47bd8ab8de416d8
rodolfo <rodoufu@gmail.com>
2017-11-02 12:38:10 -0200

    Ignore list

--- code/dvnd-df/test/test_localserach.py
+++ code/dvnd-df/test/test_localserach.py
@@ -12,6 +12,6 @@ from localsearch import *
 class LocalSearchTest(unittest.TestCase):
 	def test_bestImprovement(self):
 		s = Solution(10)
-		self.assertEquals(330, sol.value, "Valor inicial")
+		self.assertEquals(330, s.value, "Valor inicial")
 		self.assertEquals(285, bestImprovement(s, lambda sol, x, y: sol.swap(x, y), lambda sol, x, y: sol.swap(x, y), False)[0].value, "BestImprovement Swap")
 		self.assertEquals(235, bestImprovement(s, lambda sol, x, y: sol.invert(x, y), lambda sol, x, y: sol.invert(x, y), False)[0].value, "BestImprovement Invert")
