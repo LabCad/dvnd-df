6b90f9c33689a8e628551dcfef4d2585eeeca401
rodolfo <rodoufu@gmail.com>
2017-11-15 10:53:35 -0200

    Funcionando com Solução e movimentos

--- code/dvnd-df/src/main.py
+++ code/dvnd-df/src/main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from copy import *
 from include_lib import *
 from solution import *
 from solution_movement import *
@@ -11,11 +10,11 @@ include_pydf()
 from pyDF import *
 
 
-def op1(arg):
-	sol = copy(arg[0])
+def op_param(movtype, arg):
+	sol = (arg[0]).gen_solution()
 	best_move = None
-	best_move_value = sol.value
-	mov = Movement(MovementType.SWAP)
+	best_move_value = ini_sol_value = sol.value
+	mov = Movement(movtype)
 	for i in xrange(len(sol)):
 		for j in xrange(i + 1, len(sol)):
 			mov.x, mov.y = i, j
@@ -25,45 +24,34 @@ def op1(arg):
 				best_move_value = actual_val
 				best_move = Movement(mov.movtype, mov.x, mov.y)
 			sol.accept(mov)
-	if best_move is not None:
-		# cp = copy(sol)
-		# sol = copy(sol)
-		sol.accept(best_move)
-		# print "Op 1 - ", cp, sol
 
-	# print "Op 1 - ", sol if len(arg) > 0 else ""
-	return sol
+	if best_move is not None:
+		solmov = SolutionMovement(sol, best_move, best_move_value - ini_sol_value)
+		if arg[0].can_merge(solmov):
+			arg[0].merge(solmov)
+		else:
+			resp = SolutionMovementCollection(sol)
+			resp.merge(solmov)
+			return resp
+
+	return arg[0]
+
+
+def op1(arg):
+	return op_param(MovementType.SWAP, arg)
 
 
 def op2(arg):
-	sol = copy(arg[0])
-	best_move = None
-	best_move_value = sol.value
-	mov = Movement(MovementType.TWO_OPT)
-	for i in xrange(len(sol)):
-		for j in xrange(i + 1, len(sol)):
-			mov.x, mov.y = i, j
-			sol.accept(mov)
-			actual_val = sol.value
-			if actual_val < best_move_value:
-				best_move_value = actual_val
-				best_move = Movement(mov.movtype, mov.x, mov.y)
-			sol.accept(mov)
-	if best_move is not None:
-		# cp = copy(sol)
-		# sol = copy(sol)
-		sol.accept(best_move)
-		# print "Op 1 - ", cp, sol
-
-	# print "Op 1 - ", sol if len(arg) > 0 else ""
-	return sol
+	return op_param(MovementType.TWO_OPT, arg)
 
 
 def assist(args):
-	print "Solution %s" % args[0], args[1]
+	# print "Solution %s - %s" % (args[0], args[1])
+	# print "final %s - %s" % (args[0].gen_solution(), args[1].gen_solution())
 
-	if args[1] < args[0] or len(args[0]) == 0:
-		return args[1]
+	if args[0].value is None or args[1] < args[0]:
+		print "sol mov %s - %s" % (args[1], args[1].gen_solution())
+		return deepcopy(args[1])
 	else:
 		# print "Parou2 ", args[0], args[1]
 		return False
@@ -72,14 +60,11 @@ def assist(args):
 graph = DFGraph()
 
 emptySol = Solution(0)
-iniSol = Solution(10, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-best_sol = copy(iniSol)
-# print "emptySol ", emptySol
-# iniSol.rand()
+iniSol = Solution(50)
 print "iniSol ", iniSol
 
-ini = Feeder(iniSol)  # -1 is the initial value of the first input of the FliFlop node, to force it propagate the initial solution
-ini2 = Feeder(emptySol)  # 100 is the initial solution
+ini = Feeder(SolutionMovementCollection(iniSol))  # -1 is the initial value of the first input of the FliFlop node, to force it propagate the initial solution
+ini2 = Feeder(SolutionMovementCollection(emptySol))  # 100 is the initial solution
 
 heurSize = 2
 
@@ -87,22 +72,22 @@ heur = [Node(eval("op%d" % i), 1) for i in xrange(1, heurSize + 1)]
 
 assist1 = FlipFlop(assist)
 
-for i in xrange(heurSize):
-	graph.add(heur[i])
+for h in heur:
+	graph.add(h)
 graph.add(ini)
 graph.add(ini2)
 
 graph.add(assist1)
 
-for i in xrange(heurSize):
-	heur[i].add_edge(assist1, 1)
+for h in heur:
+	h.add_edge(assist1, 1)
 
-for i in xrange(heurSize):
-	assist1.add_edge(heur[i], 0)
+for h in heur:
+	assist1.add_edge(h, 0)
 assist1.add_edge(assist1, 0)
 
-for i in xrange(heurSize):
-	ini.add_edge(heur[i], 0)
+for h in heur:
+	ini.add_edge(h, 0)
 
 ini.add_edge(assist1, 0)
 ini2.add_edge(assist1, 1)
@@ -111,10 +96,3 @@ ini2.add_edge(assist1, 1)
 
 sched = Scheduler(graph, 5, mpi_enabled=False)
 sched.start()
-
-# a = Solution(10, [8, 9, 1, 7, 6, 3, 2, 4, 5, 0])
-# b = op1([a])
-# print "fim (%s, %s, %s) " % (a, b, op1([b]))
-#
-# c = Solution(10, [9, 8, 1, 7, 6, 3, 2, 4, 5, 0])
-# print "fim2 (%s, %s) " % (a, c)
--- code/dvnd-df/src/solution_movement.py
+++ code/dvnd-df/src/solution_movement.py
@@ -8,6 +8,9 @@ class SolutionMovement:
 		self.mov = mov
 		self.imp = imp
 
+	def __str__(self):
+		return "{ mov: %s, imp: %s}" % (self.mov, self.imp)
+
 	def can_merge(self, other):
 		return self.sol == other.sol and self.mov.not_conflict(other.mov)
 
@@ -17,11 +20,18 @@ class SolutionMovementCollection:
 		self.__sol = copy(sol)
 		self.__movs = {}
 		self.__solvalue = sol.value
-		self.__value = None
+		self.__value = 0
+
+	def __lt__(self, other):
+		return self.value < other.value
+
+	def __str__(self):
+		return "{ solution: %s, movs: %s, value: %d%+d=%d }" % \
+			(self.__sol, [x for x in self.__movs], self.__solvalue or 0, self.__value, self.value or 0)
 
 	@property
 	def value(self):
-		return self.__solvalue + (self.__value if self.__value is not None else sum([value for key, value in self.__movs.iteritems()]))
+		return (self.__solvalue + self.__value) if self.__solvalue is not None else None
 
 	def can_merge(self, solmov):
 		if self.__sol != solmov.sol:
@@ -37,4 +47,16 @@ class SolutionMovementCollection:
 	def merge(self, solmov):
 		if self.can_merge(solmov):
 			self.__movs[solmov.mov] = solmov.imp
-			self.__value = None
+			self.__value += solmov.imp
+
+	def ini_solution_value(self):
+		return self.__sol.value
+
+	def ini_solution(self):
+		return copy(self.__sol)
+
+	def gen_solution(self):
+		sol = copy(self.__sol)
+		for x in self.__movs:
+			sol.accept(x)
+		return sol
