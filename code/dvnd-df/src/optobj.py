c80f871282232d1cd344af43cfa61d0306ac0757
rodolfo <rodoufu@gmail.com>
2018-01-12 00:50:33 -0200

    Tentando funcionar

--- code/dvnd-df/src/optobj.py
+++ code/dvnd-df/src/optobj.py
@@ -3,18 +3,19 @@ from pydf import *
 
 
 class DecisionNode(Node):
-	def __init__(self, f, inputn, should_run, keep_going):
+	def __init__(self, f, inputn, should_run=lambda x: True, keep_going=lambda x, y: True):
 		Node.__init__(self, f, inputn)
 		self.__should_run = should_run
 		self.__keep_going = keep_going
 
 	def run(self, args, workerid, operq):
-		if not self.__should_run(args):
+		param_args = [a.val for a in args]
+		if not self.__should_run(param_args):
 			self.sendops([Oper(workerid, None, None, None)], operq)
 		else:
-			resp = self.f([a.val for a in args])
+			resp = self.f(param_args)
 
-			if not self.__keep_going(args, resp):
+			if not self.__keep_going(param_args, resp):
 				opers = [Oper(workerid, None, None, None)]
 			else:
 				opers = self.create_oper(resp, workerid, operq)
@@ -23,16 +24,36 @@ class DecisionNode(Node):
 
 
 class OptMessage:
-	def __init__(self, solmap, source):
+	def __init__(self, solmap={}, source=0, target=[], not_improved=[]):
 		self.__solmap = solmap
 		self.__source = source
+		self.__target = target
+		self.__not_improved = not_improved
 
 	def __getitem__(self, item):
 		return self.__solmap[item]
 
-	def getbest(self):
+	def __setitem__(self, item, val):
+		self.__solmap[item] = val
+
+	def get_best(self):
 		return min(self.__solmap.values())
 
 	@property
 	def source(self):
 		return self.__source
+
+	def has_target(self, idx):
+		return self.__target[idx]
+
+	def has_not_improved(self, idx):
+		return not self.__not_improved[idx]
+
+	def no_improvement(self):
+		return all(self.__not_improved)
+
+	def unset_all_targets(self):
+		self.__target = [False for x in self.__target]
+
+	def set_target(self, idx, val=True):
+		self.__target[idx] = val
--- code/dvnd-df/src/other.py
+++ code/dvnd-df/src/other.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from include_lib import *
-from  optobj import *
+from optobj import *
 from solution import *
 from solution_movement import *
 
@@ -13,32 +13,64 @@ include_pydf()
 from pyDF import *
 
 
+def fim(args):
+	print "Fim - len: {0} - {1}".format(len(args), args)
+	return args[0]
+
+
+def manager(args):
+	print "Manager - len: {0} - {1}".format(len(args), args)
+	atual = args[0]
+	melhor = args[1]
+	melhor.unset_all_targets()
+	if atual[atual.source] < melhor[atual.source]:
+		melhor[atual.source] = atual[atual.source]
+		melhor.set_target(atual.source)
+	melhor[atual.source] = melhor.get_best()
+	return melhor
+
+
+def oper_n(args, n):
+	print "oper{0} - len: {1} - {2}".format(n, len(args), args)
+	args[0].source = n
+	args[0][n] = 9 if n != 0 else 8
+	return args[0]
+
+
+def test_oper(args, n):
+	print "test_oper{0} - len: {1} - {2}".format(n, len(args), args)
+	return args[0].has_target(n)
+
+
+numberOfWorkers = 2
+numberOfOpers = 2
+initialSolution = 10
+
 graph = DFGraph()
-heurSize = 2
 
-inisol = None
+fimNode = DecisionNode(fim, 1, lambda x: x[0].no_improvement())
+graph.add(fimNode)
 
-inicial = [Feeder(OptMessage({x: inisol}, x)) for x in xrange(heurSize + 1)]
-inicialVazia = Feeder(OptMessage({}, x))
-heur = [DecisionNode(eval("ope%d" % i), 1, None, None) for i in xrange(heurSize)]
-ger = DecisionNode(None, 2, None, None)
-fim = DecisionNode(None, 1, None, None)
+manNode = DecisionNode(manager, 2)
+graph.add(manNode)
+manNode.add_edge(manNode, 1)
+manNode.add_edge(fimNode, 0)
 
-graph.add(inicialVazia)
-graph.add(ger)
-for ini in inicial:
-	graph.add(ini)
-for h in heur:
-	graph.add(h)
+iniManNode = Feeder(OptMessage({x: initialSolution for x in xrange(numberOfOpers)}, numberOfOpers, [True for i in xrange(numberOfOpers)]))
+graph.add(iniManNode)
+iniManNode.add_edge(manNode, 1)
 
-inicial[heurSize].add_edge(ger, 0)
-inicialVazia.add_edge(ger, 1)
-ger.add_edge(ger, 1)
-for i in xrange(heurSize):
-	inicial[i].add_edge(heur[i], 0)
-for h in heur:
-	ger.add_edge(h, 0)
-	h.add_edge(ger, 0)
+# operNode = [DecisionNode(lambda y: oper_n(y, i), 1, lambda y: y[0].has_target(i), lambda x, y: x[0][i] > y[i]) for i in xrange(numberOfOpers)]
+# operNode = [DecisionNode(lambda a: oper_n(a, i), 1, lambda b: test_oper(b, i), lambda x, c: x[0][i] > c[i]) for i in xrange(numberOfOpers)]
+operNode = [DecisionNode(lambda a: oper_n(a, i), 1, lambda b: test_oper(b, i)) for i in xrange(numberOfOpers)]
+for x in operNode:
+	graph.add(x)
+	manNode.add_edge(x, 0)
+	x.add_edge(manNode, 0)
 
-sched = Scheduler(graph, 4, mpi_enabled=False)
-sched.start()
+iniNode = [Feeder(OptMessage({x: initialSolution}, numberOfOpers, [x == y for y in xrange(numberOfOpers)])) for x in xrange(numberOfOpers)]
+for i in xrange(numberOfOpers):
+	graph.add(iniNode[i])
+	iniNode[i].add_edge(operNode[i], 0)
+
+Scheduler(graph, numberOfWorkers, mpi_enabled=False).start()
