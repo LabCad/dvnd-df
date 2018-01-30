7b8bcb2e1767143fa46caf9441fbc7ae4fa80f91
Igor Machado <igor.machado@gmail.com>
2018-01-30 20:23:46 -0200

    adding host_file option back again

--- code/dvnd-df/src/runmpi.sh
+++ code/dvnd-df/src/runmpi.sh
@@ -1,3 +1,3 @@
 #mpirun --hostfile host_gpu optirun python main.py -mpi -n 1 -in 1
-mpirun -np 1 optirun python main.py -mpi -in 1
+mpirun -np 1 --hostfile host_gpu optirun python main.py -mpi -in 1
 
