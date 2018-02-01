db3c5c3078c48a820436a641f88fee45e91907ac
rodolfo <rodoufu@gmail.com>
2018-01-31 22:14:44 -0200

    Contagem de tempo

--- code/dvnd-df/src/mainvnd.py
+++ code/dvnd-df/src/mainvnd.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
+import random
+import time
 os.environ['PYDF_HOME'] = "/home/imcoelho/git-reps/dvnd-df/code/dvnd-df"
 os.environ['SIMPLE_PYCUDA_HOME'] = "/home/imcoelho/git-reps/dvnd-df/code/dvnd-df/simple-pycuda"
 
 from copy import deepcopy
-#from dataflow_opt import *
 from movement import *
-from solution import Solution
-from solution_info import *
 from wraper_wamca2016 import *
 
-import random
 
 class SolutionVectorValue:
 	def __init__(self, vector, value):
@@ -27,39 +25,6 @@ class SolutionVectorValue:
 	def __str__(self):
 		return "{}-{}".format(self.value, self.vector)
 
-
-def neigh_mov(args, inimov):
-	atual = args[0]
-	# antes = atual[oper_idx]
-	# str_antes = "oper{} - sv: {}".format(oper_idx, solvalue)
-
-	# movs = {0: MovementType.SWAP, 1: MovementType.TWO_OPT, 2: MovementType.OR_OPT_K}
-	# movtype = movs[oper_idx]
-	movsinv = {MovementType.SWAP: 0, MovementType.TWO_OPT: 1, MovementType.OR_OPT_K: 2}
-	oper_idx = movsinv[inimov.movtype]
-	atual.source = oper_idx
-	sol = atual[oper_idx]
-	best_val = sol.value
-	best_sol = deepcopy(sol)
-	sol_copy = deepcopy(sol)
-	mov = inimov
-	for i in xrange(len(sol)):
-		for j in xrange(i + 1, len(sol)):
-			mov.x, mov.y = i, j
-			# TODO Melhorar implementação com desfazer movimento
-			sol_copy.set_route(sol.get_route)
-			sol_copy.accept(mov)
-			sol_val = sol_copy.value
-			if sol_val < best_val:
-				best_val = sol_val
-				best_sol.set_route(sol_copy.get_route)
-
-	atual[oper_idx] = best_sol
-	# print "{}:{}-{}".format(oper_idx, sol, best_sol)
-
-	return atual
-
-
 def neigh_gpu(args, file, inimov):
 	atual = args[0]
 	atual.source = inimov
@@ -67,11 +32,6 @@ def neigh_gpu(args, file, inimov):
 	resp = best_neighbor(file, solution.vector, inimov)
 	atual[inimov] = SolutionVectorValue(resp[0], resp[1])
 	return atual
-
-
-def print_final_solution(args):
-	print [args[0][i].value for i in xrange(3)]
-	print "Fim - best: {}".format(args[0].get_best())
 
 
 solution_index = int(0 if "-in" not in sys.argv else sys.argv[sys.argv.index("-in") + 1])
@@ -96,6 +56,7 @@ random.shuffle(neigh_op)
 print "ns: ",neigh_op
 print ini_solution
 
+start_time = time.time()
 solution = deepcopy(ini_solution)
 k = 0
 print "initial_solution=",ini_solution.value
@@ -111,5 +72,5 @@ while k < len(neigh_op):
 	else:
 		k += 1
 
-
-print "finished rvnd"
+end_time = time.time()
+print "finished rvnd in {}s".format(end_time - start_time)
