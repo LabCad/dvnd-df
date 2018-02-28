2212cba4791235fc1836c15b0418c303e5d7526f
rodolfo <rodoufu@gmail.com>
2018-02-28 00:08:56 -0300

    Resultados n1 w1-10

---
+++ doc/results/gpu03/gpu03.r
@@ -0,0 +1,16 @@
+setwd("~/git/dvnd-df/doc/results/gpu03/")
+
+n1w1rvnd = read.csv(file="n1w1rvnd.csv",head=TRUE,sep=";")
+
+n1w1dvnd = read.csv(file="n1w1dvnd.csv",head=TRUE,sep=";")
+n1w2dvnd = read.csv(file="n1w2dvnd.csv",head=TRUE,sep=";")
+n1w3dvnd = read.csv(file="n1w3dvnd.csv",head=TRUE,sep=";")
+n1w4dvnd = read.csv(file="n1w4dvnd.csv",head=TRUE,sep=";")
+n1w5dvnd = read.csv(file="n1w5dvnd.csv",head=TRUE,sep=";")
+n1w6dvnd = read.csv(file="n1w6dvnd.csv",head=TRUE,sep=";")
+n1w7dvnd = read.csv(file="n1w7dvnd.csv",head=TRUE,sep=";")
+n1w8dvnd = read.csv(file="n1w8dvnd.csv",head=TRUE,sep=";")
+n1w9dvnd = read.csv(file="n1w9dvnd.csv",head=TRUE,sep=";")
+n1w10dvnd = read.csv(file="n1w10dvnd.csv",head=TRUE,sep=";")
+
+# summary(n1w1rvnd)
