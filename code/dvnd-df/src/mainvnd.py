fd9aa70cfe627c7766a4e2d7097687ff8a2352ee
rodolfo <rodoufu@gmail.com>
2018-07-14 12:21:04 -0300

    Fixing vnd with no dataflow.

--- code/dvnd-df/src/mainvnd.py
+++ code/dvnd-df/src/mainvnd.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # from dataflow_opt import *
-from wraper_wamca2016 import *
 import random
 import time
-import sys
+import numpy
 from copy import deepcopy
+from wraper_wamca2016 import WamcaWraper, get_file_name
+from util import hasparam, getparam
 
+# Command line parameters
+solution_index = int(getparam("in", None, 0))
+solution_instance_index = int(getparam("sii", "solution_instance_index", -1))
+# solution_in_index = None if "-sn" not in sys.argv else int(sys.argv[sys.argv.index("-sn") + 1])
+multi_gpu = hasparam("mg", "multi_gpu")
+goal = getparam(None, "goal", "min").lower() == "max"
+problem_name = getparam("p", None, "ml")
+number_of_moves = int(getparam(None, "number_of_moves", 10))
+device_count = int(getparam("dc", "device_count", 1))
+solver_param = getparam("s", "solver", "dvnd").lower()
+mpi_enabled = hasparam("mpi")
+workers = int(getparam("n", None, 1))
 
 start_time = time.time()
-solution_index = int(0 if "-in" not in sys.argv else sys.argv[sys.argv.index("-in") + 1])
-sol_info = wamca_solution_instance_file[solution_index]
-file_name = wamca_intance_path + sol_info[0]
 
-solint = [x for x in xrange(sol_info[1])]
-from wraper_wamca2016 import create_initial_solution, neigh_gpu, get_file_name#, best_neighbor_moves, \
-	# get_no_conflict, merge_moves, apply_moves, calculate_value, best_neighbor
-# import numpy
 file_name = get_file_name(solution_index)
-ini_solution = create_initial_solution(solution_index)
-# ini_solution = SolutionVectorValue(solint, calculate_value(file_name, solint))
+mylib = WamcaWraper(file_name, useMultipleGpu=multi_gpu, deviceCount=device_count)
+file_name = get_file_name(solution_index)
+ini_solution = mylib.create_initial_solution(solution_index, solver_param, solution_instance_index)
 
-neigh_op = [lambda ab, y=mv: neigh_gpu(ab, file_name, y) for mv in xrange(3)]
+neigh_op = [lambda ab, y=mv: mylib.neigh_gpu(ab, y) for mv in xrange(5)]
 print "ns: ", neigh_op
 random.shuffle(neigh_op)
 print "ns: ", neigh_op
@@ -33,16 +40,19 @@ solution2 = deepcopy(solution)
 k = 0
 while k < len(neigh_op):
 	# print "Start k=", k, " f=", solution.value,
-	# solution2 = deepcopy(solution)
-	for i in xrange(len(solution.vector)):
-		solution2.vector[i] = solution.vector[i]
+	# for i in xrange(len(solution.vector)):
+	# 	solution2.vector[i] = solution.vector[i]
+	numpy.copyto(solution2.vector, solution.vector)
 	solution2.value = solution.value
-	resp = best_neighbor(file_name, solution2.vector, k)
+	# resp = best_neighbor(file_name, solution2.vector, k)
+	resp = mylib.neigh_gpu(solution2, k)
 	# print "out k=", k, " -> ", resp[1]
-	if resp[1] < solution.value:
+	if resp[0] < solution:
 		k = 0
-		solution.vector = resp[0]
-		solution.value = resp[1]
+		numpy.copyto(solution.vector, resp[0].vector)
+		# solution.vector = resp[0]
+		solution.value = resp[0].value
+		# solution.value = resp[1]
 	else:
 		k += 1
 
