0655e93dcb0af6ea2b73cb4ef11f0b5f4a0ebdf9
rodolfo <rodoufu@gmail.com>
2018-07-15 19:25:03 -0300

    Data update.

--- doc/results/gdvnd/analise.r
+++ doc/results/gdvnd/analise.r
@@ -19,7 +19,8 @@ dvndGdvndSameInitial$initialSolMethod = rep("same", length(dvndGdvndSameInitial$
 dvndGdvndRandInitial$initialSolMethod = rep("rand", length(dvndGdvndRandInitial$initial))
 rvndSameInitial$initialSolMethod = rep("rand", length(rvndSameInitial$initial))
 
-dvndGdvndData = merge(dvndGdvndSameInitial, dvndGdvndRandInitial, rvndSameInitial, all=TRUE)
+dvndGdvndData = merge(dvndGdvndSameInitial, dvndGdvndRandInitial, all=TRUE)
+# dvndGdvndData = merge(merge(dvndGdvndSameInitial, dvndGdvndRandInitial, all=TRUE), rvndSameInitial, all=TRUE)
 
 for (draw_n in c(4)) {
   for (iniMethod in c("same", "rand")) {
@@ -29,30 +30,30 @@ for (draw_n in c(4)) {
       library(ggplot2)
       for (draw_w in 4:5) {
         data_src = dvndGdvndData[inum == draw_inum & n == draw_n & w == draw_w & initialSolMethod == iniMethod][order(solver, time)]
-        # if (length(data_src$sample) > 0) {
-        #   x_axis_value = rep(1:100, length(data_src$sample) / 100)
-        #   
-        #   mychart = ggplot(data=data_src, aes(x=x_axis_value, y=time, group=solver)) 
-        #   mychart = mychart + geom_line(aes(color=solver))
-        #   mychart = mychart + geom_point(aes(color=solver))
-        #   mychart = mychart + scale_x_discrete(name ="sample")
-        #   mychart = mychart + ggtitle(paste(iniMethod, " initial - Time in", draw_inum, "n", draw_n, "w", draw_w, sep=""))
-        # 
-        #   ggsave(paste("chart/", iniMethod, "_time_in", draw_inum, "n", draw_n, "w", draw_w, sep=""), plot = mychart, device="png")
-        # }
-        # 
-        # data_src = data_src[order(solver, imp)]
-        # if (length(data_src$sample) > 0) {
-        #   x_axis_value = rep(1:100, length(data_src$sample) / 100)
-        #   mychart = ggplot(data=data_src, aes(x=x_axis_value, y=imp, group=solver))
-        # 
-        #   mychart = mychart + geom_line(aes(color=solver))
-        #   mychart = mychart + geom_point(aes(color=solver))
-        #   mychart = mychart + scale_x_discrete(name ="sample")
-        #   mychart = mychart + ggtitle(paste(iniMethod, " initial - Improvement in", draw_inum, "n", draw_n, "w", draw_w, sep=""))
-        # 
-        #   ggsave(paste("chart/", iniMethod, "_imp_in", draw_inum, "n", draw_n, "w", draw_w, sep=""), plot = mychart, device="png")
-        # }
+        if (length(data_src$sample) > 0) {
+          x_axis_value = rep(1:100, length(data_src$sample) / 100)
+
+          mychart = ggplot(data=data_src, aes(x=x_axis_value, y=time, group=solver))
+          mychart = mychart + geom_line(aes(color=solver))
+          mychart = mychart + geom_point(aes(color=solver))
+          mychart = mychart + scale_x_discrete(name ="sample")
+          mychart = mychart + ggtitle(paste(iniMethod, " initial - Time in", draw_inum, "n", draw_n, "w", draw_w, sep=""))
+
+          ggsave(paste("chart/", iniMethod, "_time_in", draw_inum, "n", draw_n, "w", draw_w, ".png", sep=""), plot = mychart, device="png")
+        }
+
+        data_src = data_src[order(solver, imp)]
+        if (length(data_src$sample) > 0) {
+          x_axis_value = rep(1:100, length(data_src$sample) / 100)
+          mychart = ggplot(data=data_src, aes(x=x_axis_value, y=imp, group=solver))
+
+          mychart = mychart + geom_line(aes(color=solver))
+          mychart = mychart + geom_point(aes(color=solver))
+          mychart = mychart + scale_x_discrete(name ="sample")
+          mychart = mychart + ggtitle(paste(iniMethod, " initial - Improvement in", draw_inum, "n", draw_n, "w", draw_w, sep=""))
+
+          ggsave(paste("chart/", iniMethod, "_imp_in", draw_inum, "n", draw_n, "w", draw_w, ".png", sep=""), plot = mychart, device="png")
+        }
         
         data_src = data_src[order(solver, count)]
         if (length(data_src$sample) > 0) {
@@ -64,7 +65,7 @@ for (draw_n in c(4)) {
           mychart = mychart + scale_x_discrete(name ="sample")
           mychart = mychart + ggtitle(paste(iniMethod, " initial - Count in", draw_inum, "n", draw_n, "w", draw_w, sep=""))
           
-          ggsave(paste("chart/", iniMethod, "_count_in", draw_inum, "n", draw_n, "w", draw_w, sep=""), plot = mychart, device="png")
+          ggsave(paste("chart/", iniMethod, "_count_in", draw_inum, "n", draw_n, "w", draw_w, ".png", sep=""), plot = mychart, device="png")
         }
       }
     }
