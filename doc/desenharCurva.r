a6cb64fe2e5b2a0a44466b8a3ca31ec9e030d0a8
rodolfo <rodoufu@gmail.com>
2018-09-08 08:03:39 -0300

    Images for presentation.

---
+++ doc/desenharCurva.r
@@ -0,0 +1,10 @@
+# plot(-4:4, -4:4, type = "n")# setting up coord. system
+polynomial(c(1, -1, 1, 2, -1, 2, 3)) %>%
+  plot(main="Espaço de busca explorado", xlab="Iterações", ylab="f(x)",
+       xaxt='n', yaxt='n',
+       xlim=c(-1.1, .5))
+minimoLocal = c(-.960, .98)
+points(c(minimoLocal[1]), c(minimoLocal[2]), col = "red")
+points(c(minimoLocal[1]), c(minimoLocal[2]), col = "green", cex=13)
+points(c(.2785), c(.8425), col = "blue")
+legend(.25, 1.4, legend=c("Mínimo local", "Vizinhança", "Ótimo"), col=c("red", "green", "blue"), lty=1:1, cex=0.8)
