e251bc7d42e537152d867960649230f417e9715d
Rodolfo Araujo <rodoufu@gmail.com>
2019-12-14 18:51:40 -0300

    Running

--- dvnd_df/script/runexp.sh
+++ dvnd_df/script/runexp.sh
@@ -9,9 +9,9 @@ if [ $# -gt 3 ]; then
 	solver_name=$4
 	file_list=()
 	problem=
-	home_p="/home/rodolfo/git/dvnd-df/"
-	home_script=$home_p"code/dvnd-df/script/"
-	home_src=$home_p"code/dvnd-df/dvnd-df/"
+	home_p="/home/rodolfo/parco/dvnd-df/"
+	home_script=$home_p"dvnd_df/script/"
+	home_src=$home_p"dvnd_df"
 	home_doc=$home_p"doc/"
 
 	if [ $# -lt 5 ]; then
@@ -34,7 +34,7 @@ if [ $# -gt 3 ]; then
 			file_list=(1 2 5 6)
 		fi
 		if [ "$host_num" = "1_4" ]; then
-			file_list=(0..7)
+			file_list=(0..50)
 		fi
 	else
 		file_numbers="inicio $5 fim"
@@ -69,18 +69,28 @@ if [ $# -gt 3 ]; then
 			if [ "$solver_name" = "dvnd_sog" -o "$solver_name" = "gdvnd_sog" ]
 			then
 				echo "SOG-"$solver_name
-				time mpirun -np $num_proc --hostfile $home_script""$host_file python $home_src"main.py" -sii $i -mpi -sog -n $num_workes -in $filei -mg -dc 2 -s ${solver_name:0:4} -p ml > $home_doc"results/"$file_name".out" 2> $home_doc"results/"$file_name".log"
+				time mpirun.mpich -np $num_proc --hostfile $home_script""$host_file python $home_src"main.py" -sii $i -mpi -sog -n $num_workes -in $filei -mg -dc 2 -s ${solver_name:0:4} -p ml > $home_doc"results/"$file_name".out" 2> $home_doc"results/"$file_name".log"
 			elif [ "$solver_name" = "rvnd_no_mpi" ]
 			then
 				echo "rvnd_no_mpi-$solver_name"
 				temp_solver_name="rvnd"
 				python $home_src"main.py" -n $num_workes -sii $i -in $filei -s $temp_solver_name -p $problem > $home_doc"results/"$file_name".out" 2> $home_doc"results/"$file_name".log"
 			else
-				echo "else-"$solver_name
-				time mpirun -np $num_proc --hostfile $home_script""$host_file python $home_src"main.py" -sii $i -mpi -n $num_workes -in $filei -mg -dc 2 -s $solver_name -p ml > $home_doc"results/"$file_name".out" 2> $home_doc"results/"$file_name".log"
+				file_out=$home_doc"results/"$file_name
+				echo "else-"$solver_name" to "$file_out
+				# python "${home_src}/main.py"
+				#time mpirun.mpich -np $num_proc --hostfile $home_script""$host_file python $home_src"/main.py" -sii $i -mpi -n $num_workes -in $filei -mg -dc 2 -s $solver_name -p ml > $file_out".out" 2> $file_out".log"
+				time mpirun.mpich -np $num_proc --hostfile $home_script""$host_file python $home_src"/main.py" -sii -2 -mpi -n $num_workes -in $filei -dc 2 -s $solver_name -p ml > $file_out".out" 2> $file_out".log"
 			fi
 		done
 	done
+	
+	echo "num_proc="$num_proc", num_workes="$num_workes", host_num="$host_num", host_file="$host_file
+	echo "solver_name="$solver_name", file_list="$file_list" problem="$problem
+	echo "home_p="$home_p
+	echo "home_script="$home_script
+	echo "home_src="$home_src
+	echo "home_doc="$home_doc
 else
 	echo "Use: $0 <num_proc> <num_workes> <host_num> <solver_name> [<file_list>] [<problem>]"
 fi
